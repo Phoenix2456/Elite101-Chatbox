9d8dff0f50ab27c9908b5462b15c508096696918
Benny <phoenixshadow2456@gmail.com>
2024-09-26 05:00:49 +0000

    Asked the user to choose from the following menu

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ print(welcome)
 name = input("What's your name? ")
 age = int(input(f"Nice to meet you {name}. How old are you? "))
 print (input("How may I be of help to you today?"))
+
+print("Please choose from the following: ")
