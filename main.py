75e2393e36d69e1ea64568faed5625c5a19c5867
Benny <phoenixshadow2456@gmail.com>
2024-09-26 04:17:30 +0000

    Removed welcome message previously on line 4

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 welcome = "Welcome to Elite 101 Chatbot "
 print(welcome)
 name = input("What's your name? ")
-print(f"Welcome {name}")
 age = int(input(f"Nice to meet you {name}. How old are you? "))
 print (input("How may I be of help to you today?"))
