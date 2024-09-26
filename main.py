f0a25d2e928477d2045e4ac0f9461c6e078af260
Benny <phoenixshadow2456@gmail.com>
2024-09-26 04:05:13 +0000

    asked the user's age

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ welcome = "Welcome to Elite 101 Chatbot "
 print(welcome)
 name = input("What's your name? ")
 print(f"Welcome {name}")
+age = int(input(f"Nice to meet you {name}. How old are you? "))
