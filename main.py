4e510c2ed55ede279074bf418ed92db0dc923592
Benny <phoenixshadow2456@gmail.com>
2024-09-26 03:53:42 +0000

    added a welcome message

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-welcome = "Welcome to Elite 101 Chatbot"
+welcome = "Welcome to Elite 101 Chatbot "
+name = input("What's your name? ")
+age = int(input("How old are you? "))
+print(welcome)
+print(f"{welcome} {name}")
+
 
