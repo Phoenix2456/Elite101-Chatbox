c2afa8f95a7e240ee4d01414269e85b28105c0c3
Benny <phoenixshadow2456@gmail.com>
2024-09-27 05:39:16 +0000

    Fixed an error on line 7 - menu Fixed problem on for loop Removed the input function on line 5

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ welcome = "Welcome to Elite 101 Chatbot "
 print(welcome)
 name = input("What's your name? ")
 age = int(input(f"Nice to meet you {name}. How old are you? "))
-print (input("How may I be of help to you today?"))
+print ("How may I be of help to you today?")
 
-menu = []
+menu = [
     "1. Placeholder Option 1", 
     "2. Placeholder Option 2",
     "3. PLaceholder Option 3", 
     "4. Exit the conversation"
 ]
-print(menu)
+
 
 print("Please choose from the following: ")
+for f in menu:
+    print(f)
