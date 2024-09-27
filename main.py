45c74ebce429a66a96856f0be4fdda2ecb6fcc5e
Benny <phoenixshadow2456@gmail.com>
2024-09-27 05:30:30 +0000

    Added the menu using dictionaries

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ age = int(input(f"Nice to meet you {name}. How old are you? "))
 print (input("How may I be of help to you today?"))
 
 print("Please choose from the following: ")
+menu = {
+    "1. Placeholder Option 1", 
+    "2. Placeholder Option 2",
+    "3. PLaceholder Option 3", 
+    "4. Exit the conversation"
+}
