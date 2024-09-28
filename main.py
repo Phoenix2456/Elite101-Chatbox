fbdf1e893bedb2aa2d1a7224baf969267ef11ef8
Benny <phoenixshadow2456@gmail.com>
2024-09-28 00:14:32 +0000

    Fixed problems from line 7 - 32

--- main.py
+++ main.py
@@ -4,25 +4,29 @@ name = input("What's your name? ")
 age = int(input(f"Nice to meet you {name}. How old are you? "))
 print ("How may I be of help to you today?")
 
-menu = [
+def menu():
+  choices = [
     "1. Placeholder Option 1", 
     "2. Placeholder Option 2",
     "3. PLaceholder Option 3", 
     "4. Exit the conversation"
-]
+  ]
+  
+  print("Please choose from the following")
+  for options in choices:
+    print(options)
+  choices = input("Enter your number choice: ")
+  
+  if choices == "1":
+      print("Choice not available")
+  elif choices == "2":
+      print("Choice not available")
+  elif choices == "3":
+      print("Choice not available")
+  elif choices == "4":
+      print(f"Have a great day {name}")
+  else:
+    print("Please pick a number from the menu")
+    
 
-print("Please choose from the following: ")
-for f in menu:
-    print(f)
-choice = int(input("Enter your number choice: "))
-
-if choice == "1.":
-    print("Choice not available")
-elif choice == "2.":
-    print("Choice not available")
-elif choice == "3.":
-    print("Choice not available")
-elif choice == "4.":
-    print(f"Have a great day {name}")
-else:
-    print("Please choose a number")
+menu()
